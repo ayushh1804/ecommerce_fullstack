ab8e187fbc504e5f3c4f35fa922bcde11c1081dd
AYUSH <ayushrajbls@gmail.com>
2023-03-03 20:19:57 +0530

    api success

--- models/products.js
+++ models/products.js
@@ -1,12 +1,13 @@
 import { model, models, Schema } from "mongoose";
 
-const ProductSchema = new Schema (definition : {
-    name : String,
-    description : String,
-    price : String,
-    category : String,
-    picture : String,
-    });
+const ProductSchema = new Schema({
+  name: String,
+  description: String,
+  price: String,
+  category: String,
+  picture: String,
+});
 
-    const Product = models? .Product || model(name : 'Product', ProductSchema)
-    export default Product;
+const Product = models?.Product || model('Product', ProductSchema);
+
+export default Product;
--- pages/api/products.js
+++ pages/api/products.js
@@ -1,8 +1,9 @@
 
 import initMongoose from "@/lib/mongoose"
+import Product from "@/models/products";
 const  handler = async (req , res) => {
  await initMongoose();
- 
+    res.json( await Product.find().exec())
 }
 
 export default handler
