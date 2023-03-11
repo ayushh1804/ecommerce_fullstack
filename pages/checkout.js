0ef6e8249b33b99c51f8f60c940dcff8f85af67f
AYUSH <ayushrajbls@gmail.com>
2023-03-11 22:58:14 +0530

    cart page

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Layout from '@/components/Layout'
+import { ProductsContext } from '@/components/ProductsContext'
 const checkout = () => {
+
+  const { selectedProducts } = useContext(ProductsContext)
+  const [productinfo, setProductInfo] = useState([])
+  useEffect(() => {
+    fetch('/api/products?ids=' + selectedProducts.join(','))
+      .then(response => response.json())
+      .then(json => setProductInfo(json));
+  })
   return (
-   <Layout>
-    checkout
-   </Layout>
+    <Layout>
+      {selectedProducts.join(',')}
+    </Layout>
   )
 }
 
