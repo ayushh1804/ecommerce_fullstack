4a41c5ad304dedf4e6602e8795e563e0531cde6c
AYUSH <ayushrajbls@gmail.com>
2023-03-12 12:05:44 +0530

    changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import image from "../public/products/logo.png"
 import Image from 'next/image'
 const Navbar = () => {
     return (
-        <div className='flex justify-around align-middle  bg-slate-400 rounded p-2 font-bold' >
+        <div className='flex justify-around align-middle  bg-black text-white rounded p-2 font-bold' >
 
 
             <div> <Image src={image} className='w-24 mt-1 ml-4' /></div>
@@ -12,7 +12,7 @@ const Navbar = () => {
                 <p className='cursor-pointer mx-16'>New Arrivals</p>
                 <p className='cursor-pointer mx-16'>categories</p>
             </div >
-            <div><button className='border p-2 hover:bg-slate-300 rounded-lg'>Signin</button></div>
+            <div><button className='border p-2 hover:bg-slate-700 rounded-lg'>Signin</button></div>
         </div >
 
 
--- components/Products.js
+++ components/Products.js
@@ -7,9 +7,9 @@ const Products = ({ _id, name, price, description, picture }) => {
     }
     return (
         <div className='w-full'>
-            <h1 className='text-2xl mb-4'>{name}</h1>
-            <img src={picture} alt="" className=' bg-blue-100 p-5 rounded-xl' />
-            <h3 className='font-bold mt-2 text-lg'>{description}  </h3>
+            <h1 className='text-lg mb-4 ml-20'>{name}</h1>
+            <img src={picture} alt="" className=' bg-blue-100 p-5 rounded-xl w-80' />
+            <h3 className='font-semibold text-gray-500 mt-2 text-lg'>{description}  </h3>
             <div className='flex mt-1'>
                 <p className='text-2xl font-bold grow mt-1'>${price}</p>
                 <button onClick={addProduct} className='bg-emerald-400 px-3 py-1 rounded-xl text-white'>+</button>
